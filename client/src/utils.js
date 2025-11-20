e50ca3206b6fcd36cea7d17d636a8dfc024ed234
zaselalk <43272744+zaselalk@users.noreply.github.com>
2025-11-20 20:20:42 +0530

    :recycle: Update currency format in formatCurrency utility function

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,3 +1,3 @@
 export const formatCurrency = (amount) => {
-  return `LKR ${new Intl.NumberFormat('en-US', { style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(amount)}`;
+  return `Rs. ${new Intl.NumberFormat('en-US', { style: 'decimal', minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(amount)}`;
 };
